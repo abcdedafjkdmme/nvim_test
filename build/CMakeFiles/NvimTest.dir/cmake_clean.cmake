d7d0367f7621428aeee31fe90fddbc0828e5084b
abrka <ifradnashit@gmail.com>
2024-01-24 09:34:49 +0600

    init

---
+++ build/CMakeFiles/NvimTest.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NvimTest.dir/main.cpp.obj"
+  "CMakeFiles/NvimTest.dir/main.cpp.obj.d"
+  "NvimTest.exe"
+  "NvimTest.exe.manifest"
+  "NvimTest.lib"
+  "NvimTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NvimTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/NvimTest.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for NvimTest.
+# This may be replaced when dependencies are built.
